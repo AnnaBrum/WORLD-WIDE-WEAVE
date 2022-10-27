ac8884252a292e9092a676afc5f0b0459b301419
AnnaBrum <annahelenabrumark@gmail.com>
2022-10-27 20:44:51 +0200

    Made a nav-bar

--- index.php
+++ index.php
@@ -2,12 +2,29 @@
 
 declare(strict_types=1);
 require __DIR__ . '/header.php';
-require __DIR__ . '/data.php';
 require __DIR__ . '/functions.php';
+require __DIR__ . '/arrays.php';
 
 ?>
 
 <main>
     <h1>WORLD WIDE WEAVE</h1>
+
+    <nav>
+        <ul>
+            <li>
+                <a href=" ">Weaving and Coding </a>
+            </li>
+            <li> 
+                <a href=" "> Common patterns </a>  
+            </li>
+            <li> 
+                <a href=" "> Pattern-maker </a>  
+            </li>
+            <li> 
+                <a href=" "> Info-links </a>  
+            </li>
+        </ul>
+    </nav>
 </main>
 <?php require __DIR__ . '/footer.php'; ?>
